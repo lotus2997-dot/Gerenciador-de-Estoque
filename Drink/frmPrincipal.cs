08b2a4ee90f12c0cae44fcb1fc3e8d1fd903aa4b
GustavoOliveira-Creator <gusstavopereira365@gmail.com>
2026-04-20 18:07:22 -0300

    Add files via upload

---
+++ Drink/frmPrincipal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Drink
+{
+    public partial class frmPrincipal : Form
+    {
+        private string _usuarioLogado = "";
+        //Passando valores
+
+
+        public frmPrincipal(string usuarioDoLogin)
+        {
+            InitializeComponent();
+            _usuarioLogado = usuarioDoLogin; //Escopo local
+
+
+            lblUsuario.Text = "Bem vindo" + _usuarioLogado + "!";
+
+        }
+
+        private void frmPrincipal_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void panelMenu_Paint(object sender, PaintEventArgs e)
+        {
+            this.BackColor = Color.FromArgb(255, 87, 51);
+        }
+    }
+}
